d7fedfa73cc982965cbb98dbb9603d57181f2af0
gehirn <st.assassinka@gmail.com>
2011-12-03 15:50:38 +0200

    jacobi algorithm implementation

--- src/model/tasks/jacobi_task.cpp
+++ src/model/tasks/jacobi_task.cpp
@@ -6,24 +6,53 @@ JacobiTask::JacobiTask(const Matrix &initialMatrix, int computationAccuracy)
 {
 }
 
+qreal sign(qreal value)
+{
+    if (value > 0) return 1;
+    if (value < 0) return -1;
+    return 0;
+}
+
 void JacobiTask::run()
 {
     startTask();
 
     //output
-    Matrix eigenvaluesMatrix(initialMatrix().M(), 1);
+    EditableMatrix eigenvaluesMatrix(initialMatrix().M(), 1);
     int iterationsNumber = 0;
     qreal resultAccuracy = HUGE_VALUE;
 
+    if (!initialMatrix().isSymmetric())
     {
-/*
-	ToDo: insert your solution code here
-*/
-	mutex.lock();
-	QWaitCondition condition;
-	condition.wait(&mutex, qrand() % 1000);
-	mutex.unlock();
+	throw tr("Matrix is not symmetric.");
     }
 
+    Matrix A(initialMatrix());
+
+    for (;;)
+    {
+	Matrix::Element maxElementIndex = A.maximalNondiagonalElementIndex();
+	resultAccuracy = qAbs(A.element(maxElementIndex.i, maxElementIndex.j));
+	if (resultAccuracy < epsilon()) break;
+
+	++iterationsNumber;
+	double mu = 2 * A.element(maxElementIndex.i, maxElementIndex.j) / (A.element(maxElementIndex.i, maxElementIndex.i) - A.element(maxElementIndex.j, maxElementIndex.j));
+	MatrixElement c, s;
+	c = qSqrt((1 + 1/qSqrt(1 + qPow(mu, 2))) * 0.5);
+	s = sign(mu) * qSqrt((1 - 1/qSqrt(1 + qPow(mu, 2))) * 0.5);
+
+	EditableMatrix T(*Matrix::MatrixIdentical(A.N(), A.N()));
+	T.element(maxElementIndex.i, maxElementIndex.i) = c;
+	T.element(maxElementIndex.i, maxElementIndex.j) = -s;
+	T.element(maxElementIndex.j, maxElementIndex.i) = s;
+	T.element(maxElementIndex.j, maxElementIndex.j) = c;
+
+	A = T.transposed() * A * T;
+    }
+
+    for (Index i = 0; i < A.N(); ++i)
+    {
+	eigenvaluesMatrix.element(i, 0) = A.element(i, i);
+    }
     done(eigenvaluesMatrix, iterationsNumber, resultAccuracy);
 }

d23c4c3314e8a4463db4615f29a0556a8ce3cf3b
gehirn <st.assassinka@gmail.com>
2011-12-03 15:50:21 +0200

    qr algorithm implementation

--- src/model/tasks/qr_task.cpp
+++ src/model/tasks/qr_task.cpp
@@ -11,21 +11,65 @@ void QRTask::run()
     startTask();
 
     //output
-    Matrix eigenvaluesMatrix(initialMatrix().M(), 1);
+    EditableMatrix eigenvaluesMatrix(initialMatrix().M(), 1);
     int iterationsNumber = 0;
     qreal resultAccuracy = HUGE_VALUE;
 
-    {
-/*
-	ToDo: insert your solution code here
-*/
+    Matrix A(initialMatrix());
+    Index i, j;
 
-	mutex.lock();
-	QWaitCondition condition;
-	condition.wait(&mutex, qrand() % 1000);
-	mutex.unlock();
+    for (j = 1; j < A.N(); j++)
+    {
+	for (i = j + 1; i < A.N(); i++)
+	{
+	    MatrixElement s, c;
+	    s = A.element(i, j - 1) / qSqrt(qPow(A.element(j, j-1), 2) + qPow(A.element(i, j-1), 2));
+	    c = - A.element(j, j - 1) / qSqrt(qPow(A.element(j, j-1), 2) + qPow(A.element(i, j-1), 2));
+
+	    EditableMatrix T(*Matrix::MatrixIdentical(A.N(), A.N()));
+	    T.element(i, i) = c;
+	    T.element(i, j) = -s;
+	    T.element(j, i) = s;
+	    T.element(j, j) = c;
+
+	    A = T.transposed() * A * T;
+	}
     }
 
+    for (;;)
+    {
+	Matrix::Element maxElementIndex = A.maximalNondiagonalElementIndex();
+	resultAccuracy = qPow(A.element(maxElementIndex.i, maxElementIndex.j), 2);
+	if (resultAccuracy < epsilon()) break;
+
+	++iterationsNumber;
+	Matrix R(A);
+	Matrix Q(*Matrix::MatrixIdentical(A.N(), A.N()));
+	for (j = 0; j < A.N() - 1; j++)
+	{
+	    for (i = j + 1; i < A.N(); i++)
+	    {
+		MatrixElement s, c;
+		s = A.element(i, j) / qSqrt(qPow(A.element(j, i), 2) + qPow(A.element(i, i), 2));
+		c = - A.element(i, i) / qSqrt(qPow(A.element(j, i), 2) + qPow(A.element(i, i), 2));
+
+		EditableMatrix T(*Matrix::MatrixIdentical(A.N(), A.N()));
+		T.element(i, i) = c;
+		T.element(i, j) = -s;
+		T.element(j, i) = s;
+		T.element(j, j) = c;
+
+		R = T * R;
+		Q = Q * T.transposed();
+	    }
+	}
+	A = R * Q;
+    }
+
+    for (i = 0; i < A.N(); ++i)
+    {
+	eigenvaluesMatrix.element(i, 0) = A.element(i, i);
+    }
     done(eigenvaluesMatrix, iterationsNumber, resultAccuracy);
 }
 
